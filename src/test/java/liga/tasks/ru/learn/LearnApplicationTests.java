e22c4e399d1f01c1eaa252d9e9ed2a08b644447f
victor_krs <krasniyvictor@gmail.com>
2024-11-03 20:36:11 +0300

    initConnmmit

---
+++ src/test/java/liga/tasks/ru/learn/LearnApplicationTests.java
@@ -0,0 +1,13 @@
+package liga.tasks.ru.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
